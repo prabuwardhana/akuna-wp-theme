e2518788c54fa4c5ca4241116e353366a48e404b
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2022-02-08 22:16:42 +0900

    rename file and update corresponding hook name

--- inc/akuna-template-hooks.php
+++ inc/akuna-template-hooks.php
@@ -64,5 +64,5 @@ add_action('akuna_page', 'akuna_page_header', 10);
 add_action('akuna_page', 'akuna_page_content', 30);
 add_action('akuna_page', 'akuna_edit_post_link', 50);
 add_action('akuna_page_after', 'akuna_display_comments', 10);
-add_action('akuna_page_no_title', 'akuna_page_content', 10);
-add_action('akuna_page_no_title', 'akuna_edit_post_link', 30);
+add_action('akuna_product_page', 'akuna_page_content', 10);
+add_action('akuna_product_page', 'akuna_edit_post_link', 30);
